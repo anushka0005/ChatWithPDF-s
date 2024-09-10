1fcd2ce6ea99f467dc9013696f6c64e4346e9a42
Anushka Shukla <80688452+anushka0005@users.noreply.github.com>
2024-09-10 10:55:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Gemini PDF Chatbot
+
+Gemini PDF Chatbot is a Streamlit-based application that allows users to chat with a conversational AI model trained on PDF documents. The chatbot extracts information from uploaded PDF files and answers user questions based on the provided context.
+
+## Project Structure
+
+- `app.py`: Main application script.
+- `.env`: file which will contain your environment variable.
+- `requirements.txt`: Python packages required for working of the app.
+- `README.md`: Project documentation.
+
+## Dependencies
+
+- PyPDF2
+- langchain
+- Streamlit
+- google.generativeai
+- dotenv
---
+++ app.py
@@ -0,0 +1,127 @@
+import os
+import zipfile
+import io
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Read all PDF files from a ZIP file and return text
+def get_pdf_text_from_zip(zip_file):
+    text = ""
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        pdf_files = [f for f in zip_ref.namelist() if f.lower().endswith('.pdf')]
+        for pdf_file in pdf_files:
+            with zip_ref.open(pdf_file) as file:
+                pdf_reader = PdfReader(io.BytesIO(file.read()))
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+    return text
+
+# Split text into chunks
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+# Get embeddings for each chunk
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # type: ignore
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+# Get conversational chain
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                                   client=genai,
+                                   temperature=0.3,
+                                   )
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def clear_chat_history():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Upload a ZIP file containing PDF files and ask me a question"}]
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # type: ignore
+
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    print(response)
+    return response
+
+def main():
+    st.set_page_config(page_title="Gemini PDF Chatbot")
+
+    # Sidebar for uploading ZIP file
+    with st.sidebar:
+        st.title("Menu:")
+        zip_file = st.file_uploader("Upload a ZIP file containing PDF Files", type=["zip"])
+        if st.button("Submit & Process") and zip_file:
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text_from_zip(zip_file)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+    # Main content area for displaying chat messages
+    st.title("Chat with PDF files using Gemini")
+    st.write("Start chatting")
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+    # Chat input
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Upload a ZIP file containing PDF files and ask me a question"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
+
+if __name__ == "__main__":
+    main()
